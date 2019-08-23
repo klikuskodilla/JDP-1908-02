5bceb6eb24407564888977aaea985bf7bc89da6c
TomaszSchroeder <tomasz@schroeder.city>
2019-08-23 21:21:46 +0200

    Zadanie 12 - encja Product i zadanie 13 - encja Group v1

---
+++ src/main/java/com/kodilla/ecommercee/domain/product/Product.java
@@ -0,0 +1,15 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+public class Product {
+
+
+}
