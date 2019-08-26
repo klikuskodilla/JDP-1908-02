14b926f200eaef0af7e952b4101fa3fbf16dd469
Niedamir <rafallewandowski09@gmail.com>
2019-08-26 19:07:48 +0200

    JDP190802-15

---
+++ src/main/java/com/kodilla/ecommercee/entities/UserEntity.java
@@ -0,0 +1,64 @@
+package com.kodilla.ecommercee.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "USERS")
+public class UserEntity {
+	@Id
+	@GeneratedValue
+	@Column(name = "USER_ID", nullable = false)
+	private int id;
+	@Column(name = "FIRST_NAME", nullable = false)
+	private String firstName;
+	@Column(name = "LAST NAME", nullable = false)
+	private String lastName;
+	@Column(name = "ADRESS", nullable = false)
+	private String adress;
+	@Column(name = "PHONE", nullable = false)
+	private int phone;
+
+	public UserEntity () {}
+
+	public UserEntity(String firstName, String lastName, String adress, int phone) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.adress = adress;
+		this.phone = phone;
+	}
+
+	public int getId() {
+		return id;
+	}
+	private void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+	private void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+	private void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAdress() {
+		return adress;
+	}
+	private void setAdress(String adress) {
+		this.adress = adress;
+	}
+
+	public int getPhone() {
+		return phone;
+	}
+	private void setPhone(int phone) {
+		this.phone = phone;
+	}
+}
