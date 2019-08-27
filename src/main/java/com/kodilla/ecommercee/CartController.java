b555e3d73d64797af96c2f1ce2c3011d8421976a
ffabisiakk <ffabisiakk@gmail.com>
2019-08-27 13:46:33 +0200

    simplify controller, change mapping

--- src/main/java/com/kodilla/ecommercee/CartController.java
+++ src/main/java/com/kodilla/ecommercee/CartController.java
@@ -1,53 +1,37 @@
 package com.kodilla.ecommercee;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "1/carts", produces = "application/json")
+@RequestMapping(value = "carts", produces = "application/json")
 public class CartController {
-
-    @Autowired
-    private GenericEntityRepository cartRepository;
 
     @PostMapping()
     public void createCart() {
-        cartRepository.save(new GenericEntity());
+
     }
 
     @GetMapping("{cartId}")
-    public List<GenericEntity> getProducts(@PathVariable Long cartId) throws Exception {
-        GenericEntity cart = cartRepository.findById(cartId).orElseThrow(Exception::new);
-//        return cart.getProductList();
+    public List<GenericEntity> getProducts(@PathVariable Long cartId) {
         return new ArrayList<>();
     }
 
     @PostMapping("{cartId}/products")
-    public GenericEntity addProducts(@PathVariable Long cartId,
-                                     @RequestBody List<GenericEntity> productList) throws Exception {
-        GenericEntity cart = cartRepository.findById(cartId).orElseThrow(Exception::new);
-        for (GenericEntity product : productList) {
-//            cart.getProductList().add(product);
-        }
-        return cart;
+    public GenericEntity addProducts(@PathVariable Long cartId, @RequestBody List<GenericEntity> productList) {
+
+        return new GenericEntity("addProducts test");
     }
 
     @DeleteMapping("{cartId}/products/{productId}")
-    public boolean removeProduct(@PathVariable Long cartId,
-                                 @PathVariable Long productId) throws Exception {
-        GenericEntity cart = cartRepository.findById(cartId).orElseThrow(Exception::new);
-        GenericEntity product = cartRepository.findById(productId).orElseThrow(Exception::new);
-//        return cart.getProductList().remove(product);
+    public boolean removeProduct(@PathVariable Long cartId, @PathVariable Long productId) {
         return true;
     }
 
     @PostMapping("{cartId}/createOrder")
-    public GenericEntity createOrder(@PathVariable Long cartId) throws Exception {
-        GenericEntity cart = cartRepository.findById(cartId).orElseThrow(Exception::new);
-        GenericEntity order = new GenericEntity("cart");
-        return order;
+    public GenericEntity createOrder(@PathVariable Long cartId) {
+        return new GenericEntity("createOrder test");
     }
 }
