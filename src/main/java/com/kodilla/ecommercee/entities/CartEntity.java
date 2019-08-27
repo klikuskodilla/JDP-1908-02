734f547e12e39c00af1ffc6231124cd18d7f2b7b
Niedamir <rafallewandowski09@gmail.com>
2019-08-27 18:42:49 +0200

    JDP190802-14 fix 2 - zamiana ProductEntity na GenericEntity

--- src/main/java/com/kodilla/ecommercee/entities/CartEntity.java
+++ src/main/java/com/kodilla/ecommercee/entities/CartEntity.java
@@ -1,7 +1,10 @@
 package com.kodilla.ecommercee.entities;
 
+import com.kodilla.ecommercee.GenericEntity;
+
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "CART")
@@ -13,11 +16,11 @@ public class CartEntity {
 	@Column(name = "USER", nullable = false)
 	private UserEntity user;
 	@Column(name = "PRODUCT", nullable = false)
-	private List<ProductEntity> products = new ArrayList<>();
+	private List<GenericEntity> products = new ArrayList<>();
 
 	public CartEntity() {}
 
-	public CartEntity(UserEntity user, List<ProductEntity> products) {
+	public CartEntity(UserEntity user, List<GenericEntity> products) {
 		this.user = user;
 		this.products = products;
 	}
@@ -31,11 +34,11 @@ public class CartEntity {
 		this.user = user;
 	}
 
-	@OneToMany (targetEntity = ProductEntity.class, mappedBy = "cartEntity", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	public List<ProductEntity> getProducts() {
+	@OneToMany (targetEntity = GenericEntity.class, mappedBy = "cartEntity", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	public List<GenericEntity> getProducts() {
 		return products;
 	}
-	public void setProducts(List<ProductEntity> products) {
+	public void setProducts(List<GenericEntity> products) {
 		this.products = products;
 	}
 }
