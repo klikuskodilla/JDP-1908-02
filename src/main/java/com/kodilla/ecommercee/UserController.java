83bb42e011d437a1bbc83575ed9bb6d6a07c6755
ewaradomska <ewa.radomska@gmail.com>
2019-08-27 20:28:24 +0200

    User controller - initial implementation - poprawka

--- src/main/java/com/kodilla/ecommercee/UserController.java
+++ src/main/java/com/kodilla/ecommercee/UserController.java
@@ -1,4 +1,28 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @PostMapping("/createUser")
+    public GenericEntity createUser(){
+        return new GenericEntity();
+    }
+
+    @GetMapping("/accessDenied")
+    public String accessDenied(Long userId){
+        return "Access Denied.";
+    }
+
+    @GetMapping("/createKey")
+    public String createKey(String username,  String password){
+        return UUID.randomUUID().toString();
+    }
 }
