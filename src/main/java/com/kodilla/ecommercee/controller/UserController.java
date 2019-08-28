919085f88230b20b46c239c2fd89466cf85c2810
ewaradomska <ewa.radomska@gmail.com>
2019-08-28 21:57:01 +0200

    aktualizacja po refaktoringu projektu

--- src/main/java/com/kodilla/ecommercee/controller/UserController.java
+++ src/main/java/com/kodilla/ecommercee/controller/UserController.java
@@ -1,4 +1,27 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.entity.GenericEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+    @PostMapping("/createUser")
+    public GenericEntity createUser(){
+        return new GenericEntity();
+    }
+
+    @GetMapping("/{id}")
+    public GenericEntity getUser(Long id){
+        return new GenericEntity();
+    }
+
+    @PutMapping("/{id}/update")
+    public GenericEntity updateUser(Long id){
+        return new GenericEntity();
+    }
+
+    @DeleteMapping("/{id}/delete")
+    public void deleteUser(Long id){
+    }
 }
