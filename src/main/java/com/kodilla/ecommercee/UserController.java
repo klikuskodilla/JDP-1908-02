84d8c6369c57037d18eb2e4365ca56b41def76f7
Ewa Radomska <49087853+ewaradomska@users.noreply.github.com>
2019-08-28 14:37:36 +0200

    dodane metody getUser, deleteUser, updateUser

--- src/main/java/com/kodilla/ecommercee/UserController.java
+++ src/main/java/com/kodilla/ecommercee/UserController.java
@@ -2,6 +2,8 @@ package com.kodilla.ecommercee;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +16,20 @@ public class UserController {
     @PostMapping("/createUser")
     public GenericEntity createUser(){
         return new GenericEntity();
+    }
+    
+    @GetMapping("/{id}")
+    public GenericEntity getUser(Long id){
+        return new GenericEntity();
+    }
+    
+    @PutMapping("/{id}/update")
+    public GenericEntity updateUser(Long id){
+        return new GenericEntity();
+    }
+    
+    @DeleteMapping("/{id}/delete")
+    public void deleteUser(Long id){
     }
 
     @GetMapping("/accessDenied")
