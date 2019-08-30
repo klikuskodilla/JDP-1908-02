8046f7c30e694d1eb31854ac33b0d4e868e1d07e
lapivoinerouge <wioladys@gmail.com>
2019-08-31 01:13:09 +0200

    fixed PathVariable for ProductController

--- src/main/java/com/kodilla/ecommercee/controller/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/controller/ProductController.java
@@ -1,9 +1,8 @@
 package com.kodilla.ecommercee.controller;
 
 import com.kodilla.ecommercee.entity.GenericEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,24 +10,24 @@ import java.util.List;
 @RequestMapping("/products")
 public class ProductController {
 
-    @RequestMapping(method = RequestMethod.GET, value = "getProducts")
+    @RequestMapping(method = RequestMethod.GET, value = "/")
     public List<GenericEntity> getProducts() {
         return new ArrayList<>();
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "getProduct")
-    public GenericEntity getProduct(Long productId) {
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
+    public GenericEntity getProduct(@PathVariable("id") Long productId) {
         return new GenericEntity();
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "addProduct")
-    public void addProduct(GenericEntity genericEntity) {}
+    @RequestMapping(method = RequestMethod.POST, value = "/")
+    public void addProduct(@RequestBody GenericEntity genericEntity) {}
 
-    @RequestMapping(method = RequestMethod.PUT, value = "updateProduct")
-    public GenericEntity updateProduct(GenericEntity genericEntity) {
+    @RequestMapping(method = RequestMethod.PUT, value = "/")
+    public GenericEntity updateProduct(@RequestBody GenericEntity genericEntity) {
         return new GenericEntity();
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "deleteProduct")
-    public void deleteProduct(Long productId) {}
+    @RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
+    public void deleteProduct(@PathVariable("id") Long productId) {}
 }
