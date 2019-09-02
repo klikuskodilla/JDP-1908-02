ba9ad07d6951a9c58514431e08dc540087c15b58
ffabisiakk <ffabisiakk@gmail.com>
2019-09-02 18:39:18 +0200

    Cart Controller implementation

--- src/main/java/com/kodilla/ecommercee/controller/CartController.java
+++ src/main/java/com/kodilla/ecommercee/controller/CartController.java
@@ -1,4 +1,37 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.entity.GenericEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "carts", produces = "application/json")
 public class CartController {
+
+    @PostMapping()
+    public void createCart() {
+
+    }
+
+    @GetMapping("{cartId}")
+    public List<GenericEntity> getProducts(@PathVariable Long cartId) {
+        return new ArrayList<>();
+    }
+
+    @PostMapping("{cartId}/products")
+    public GenericEntity addProducts(@PathVariable Long cartId, @RequestBody List<GenericEntity> productList) {
+        return new GenericEntity("addProducts test");
+    }
+
+    @DeleteMapping("{cartId}/products/{productId}")
+    public boolean removeProduct(@PathVariable Long cartId, @PathVariable Long productId) {
+        return true;
+    }
+
+    @PostMapping("{cartId}/createOrder")
+    public void createOrder(@PathVariable Long cartId) {
+
+    }
 }
