70765221c4e9b644edf5ccbd7fd7e568c38fb1f9
ewaradomska <ewa.radomska@gmail.com>
2019-09-05 19:49:29 +0200

    mapper

--- src/main/java/com/kodilla/ecommercee/domain/OrderDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/OrderDto.java
@@ -1,4 +1,16 @@
 package com.kodilla.ecommercee.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderDto {
+    Long orderId;
+    LocalDate date;
+    //CartDto cart; ???
 }
--- src/main/java/com/kodilla/ecommercee/mapper/OrderMapper.java
+++ src/main/java/com/kodilla/ecommercee/mapper/OrderMapper.java
@@ -1,4 +1,36 @@
 package com.kodilla.ecommercee.mapper;
 
+import com.kodilla.ecommercee.domain.OrderDto;
+import com.kodilla.ecommercee.entity.CartEntity;
+import com.kodilla.ecommercee.entity.OrderEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
 public class OrderMapper {
+
+    public OrderEntity mapToOrder(final OrderDto orderDto){
+        return new OrderEntity(
+                orderDto.getOrderId(),
+                orderDto.getDate(),
+                new CartEntity()
+        );
+    }
+
+    public OrderDto mapToOrderDto(final OrderEntity order){
+        return new OrderDto(
+                order.getOrderId(),
+                order.getDateOfOrder()
+        //        order.getCartEntity()
+        );
+    }
+
+    public List<OrderDto> mapToOrderDtoList(final List<OrderEntity> orderList){
+        return orderList.stream()
+                .map(o -> new OrderDto(o.getOrderId(), o.getDateOfOrder()))
+                .collect(Collectors.toList());
+    }
 }
+
