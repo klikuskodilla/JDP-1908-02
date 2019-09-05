a99366c0a6f0e6675b61e0afc9c97f4d7ea1560b
davv1d <kowalskidawid63@gmail.com>
2019-09-05 19:55:45 +0200

    First commit GroupController

--- src/main/java/com/kodilla/ecommercee/service/GroupDbService.java
+++ src/main/java/com/kodilla/ecommercee/service/GroupDbService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GroupDbService {
@@ -14,5 +15,13 @@ public class GroupDbService {
 
     public List<ProductGroupEntity> getAllGroups() {
         return groupRepository.findAll();
+    }
+
+    public Optional<ProductGroupEntity> getProductGroupById(final Long groupId) {
+        return groupRepository.findById(groupId);
+    }
+
+    public ProductGroupEntity saveGroup(final ProductGroupEntity groupEntity) {
+        return groupRepository.save(groupEntity);
     }
 }
