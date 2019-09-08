3b2b3f2c8a903824b3e843690eef71fa4601c28b
lapivoinerouge <wioladys@gmail.com>
2019-09-08 19:46:21 +0200

    annotations added to repository

--- src/main/java/com/kodilla/ecommercee/repository/ProductRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/ProductRepository.java
@@ -2,10 +2,14 @@ package com.kodilla.ecommercee.repository;
 
 import com.kodilla.ecommercee.entity.ProductEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
+@Transactional
 public interface ProductRepository extends CrudRepository<ProductEntity, Long> {
 
     @Override
