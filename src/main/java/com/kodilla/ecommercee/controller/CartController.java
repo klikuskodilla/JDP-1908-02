fe2e2f462281a61e83d1872b1a0e653b770d1df6
Izabela Rojek <izabela_rojek@op.pl>
2019-09-13 20:28:51 +0200

    createOrder - impl

--- src/main/java/com/kodilla/ecommercee/controller/CartController.java
+++ src/main/java/com/kodilla/ecommercee/controller/CartController.java
@@ -2,8 +2,11 @@ package com.kodilla.ecommercee.controller;
 
 import com.kodilla.ecommercee.domain.CartDto;
 import com.kodilla.ecommercee.domain.ProductDto;
+import com.kodilla.ecommercee.entity.CartEntity;
+import com.kodilla.ecommercee.entity.OrderEntity;
 import com.kodilla.ecommercee.mapper.CartMapper;
 import com.kodilla.ecommercee.service.CartService;
+import com.kodilla.ecommercee.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,28 +21,32 @@ public class CartController {
     @Autowired
     private CartService cartService;
 
+    @Autowired
+    private OrderService orderService;
+
     @PostMapping("createCart")
     public void createCart(@RequestBody CartDto cartDto) {
         cartService.saveCart(cartMapper.mapToCartEntity(cartDto));
     }
 
     @GetMapping("{cartId}")
-    public Map<ProductDto, Integer> getProducts(@PathVariable int cartId) throws NotFoundException {
+    public Map<ProductDto, Integer> getProducts(@PathVariable Integer cartId) throws NotFoundException {
         return cartMapper.mapToProductDtoMap(cartService.getCart(cartId).orElseThrow(NotFoundException::new));
     }
 
     @PutMapping("{cartId}/products")
-    public CartDto addProducts(@PathVariable int cartId, @RequestBody Map<ProductDto, Integer> products) {
+    public CartDto addProducts(@PathVariable Integer cartId, @RequestBody Map<ProductDto, Integer> products) {
         return cartMapper.mapToCartDto(cartService.saveCart(cartService.addProducts(cartId, cartMapper.mapToProductEntityMap(products))));
     }
 
     @PutMapping("{cartId}/products/{productId}")
-    public CartDto removeProduct(@PathVariable int cartId, @PathVariable Long productId) {
+    public CartDto removeProduct(@PathVariable Integer cartId, @PathVariable Long productId) {
         return cartMapper.mapToCartDto(cartService.saveCart(cartService.deleteProduct(cartId, productId)));
     }
 
     @PostMapping("{cartId}/createOrder")
-    public void createOrder(@PathVariable int cartId) {
-
+    public void createOrder(@PathVariable Integer cartId) throws NotFoundException {
+        CartEntity cartEntity = cartService.getCart(cartId).orElseThrow(NotFoundException::new);
+        orderService.saveOrder(new OrderEntity(cartEntity));
     }
 }
